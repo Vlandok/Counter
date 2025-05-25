795665f56e1d452f26171b46bb8fa13e599f8f16
Vladislav <vlad.maskajkin@gmail.com>
2025-05-25 17:06:25 +0300

    Реализовал счетчик

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    private var currentValue: Int {
+        get {
+            return Int(counterLabel.text!)!
+        }
+        set {
+            counterLabel.text = "\(newValue)"
+        }
     }
-
-
+    
+    @IBAction func plusTappedAction() {
+        currentValue+=1
+        addHistoryMsg("значение изменено на +1")
+    }
+    
+    @IBAction func minusTappedAction() {
+        if (currentValue > 0) {
+            currentValue-=1
+            addHistoryMsg("значение изменено на -1")
+        } else {
+            addHistoryMsg("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction func resetTappedAction() {
+        currentValue = 0
+        addHistoryMsg("значение сброшено")
+    }
+    
+    private func addHistoryMsg(_ msg: String) {
+        let timestamp = DateFormatter.localizedString(
+            from: Date(),
+            dateStyle: .medium,
+            timeStyle: .medium
+        )
+        
+        historyTextView.text = historyTextView.text! + "\n\(timestamp): \(msg)"
+    }
 }
 
